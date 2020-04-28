565009a396865c2a5bad23a07f45224c09a03656
Madison Bentley <mb.madisonbentley@gmail.com>
2020-04-27 20:58:40 -0700

    moved app into sep directory

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use('/api/blogs', blogsRouter)
+
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const http = require('http')
 const express = require('express')
 const app = require('./app')
-const cors = require('cors')
+
 const mongoose = require('mongoose')
 
 console.log('connecting to mongodb')
 const mongoUrl = 'mongodb+srv://madibentl:rawDen1m@cluster0-wh8e2.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors())
-app.use(express.json())
-
 
 
 const PORT = 3003
