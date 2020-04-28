510084c5ae4428db6ef4fc1560831b27bf72bbcf
Madison Bentley <mb.madisonbentley@gmail.com>
2020-04-28 10:03:38 -0700

    implemented testing, wrote basic tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = blogs => {
   return 1
 }
@@ -10,4 +12,51 @@ const totalLikes = blogs => {
   return total
 }
 
-module.exports = {dummy, totalLikes}
+const favouriteArticle = blogs => {
+  let fav = {likes:0}
+
+  blogs.forEach(blog => {
+    if (blog.likes > fav.likes){
+      fav = blog
+    }
+  })
+  return fav
+}
+
+const mostBlogs = blogs => {
+  let blogObj = {}
+  blogs.forEach(blog => {
+    if (blogObj[blog.author]){
+      blogObj[blog.author] += 1
+    }else{
+      blogObj[blog.author] = 1
+    }
+  })
+
+  let favAuthor = Object.keys(blogObj).reduce((a, b) => blogObj[a] > blogObj[b] ? a : b);
+  favAuthor = {
+    author: favAuthor,
+    blogs: blogObj[favAuthor]
+  }
+  return favAuthor
+}
+
+const mostLikes = blogs => {
+  let blogObj = {}
+  blogs.forEach(blog => {
+    if (blogObj[blog.author]){
+      blogObj[blog.author] += blog.likes
+    }else{
+      blogObj[blog.author] = blog.likes
+    }
+  })
+
+  let maxLikes = Object.keys(blogObj).reduce((a, b) => blogObj[a] > blogObj[b] ? a : b);
+  maxLikes = {
+    author: maxLikes,
+    likes: blogObj[maxLikes]
+  }
+  return maxLikes;
+}
+
+module.exports = {dummy, totalLikes, favouriteArticle, mostBlogs, mostLikes}
